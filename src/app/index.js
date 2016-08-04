d7cf803ebd60e614781ff18be731667185d8023a
Zack Steinkamp <zs@hoopla.net>
2016-08-04 15:14:43 -0700

    fix protocol in API_PREFIX

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,7 @@ angular.module('hooplaAngularTest', ['hooplaAngularTest.models','ngAnimate', 'ng
     // visit https://app.hoopla.net/configuration/settings to provision a Client ID and Secret.
     'CLIENT_ID'     : 'CHANGEME',
     'CLIENT_SECRET' : 'CHANGEME',
-    'API_PREFIX'    : 'http://api.hoopla.net'
+    'API_PREFIX'    : 'https://api.hoopla.net'
   })
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
